dc0e5d8ee1c771eff8dbaecdba26a4494c30082e
Izzy <avrisaac555@gmail.com>
2020-01-01 17:41:31 -0800

    Updated configuration

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const Metalsmith = require("metalsmith");
 const collections = require("metalsmith-collections");
 const markdown = require("metalsmith-markdown");
 const layouts = require("metalsmith-layouts");
+const sass = require("metalsmith-sass");
 
 Metalsmith(__dirname)
     .metadata({
-        sitename: "anarchy.tech"
+        sitename: "anarchy.tech",
+        siteurl: "http://anarchy.tech/"
     })
     .source("site-source")
     .destination("build")
     .use(collections({
-        posts: "posts/*.md"
+        posts: "posts/*.md",
+    }))
+    .use(sass({
+        sourceMap: true,
+        sourceMapContents: true
     }))
     .use(markdown())
-    .use(layouts())
+    .use(layouts({
+        default: "base.pug",
+        directory: "layouts"
+    }))
