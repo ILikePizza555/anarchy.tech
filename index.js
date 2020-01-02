1005f7a76dfc781ebcb7a1f6fac147f8fcd99e1f
Izzy <avrisaac555@gmail.com>
2020-01-01 16:36:15 -0800

    More config

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Metalsmith = require("metalsmith");
+const collections = require("metalsmith-collections");
+const markdown = require("metalsmith-markdown");
+const layouts = require("metalsmith-layouts");
 
 Metalsmith(__dirname)
     .metadata({
         sitename: "anarchy.tech"
     })
+    .source("site-source")
+    .destination("build")
+    .use(collections({
+        posts: "posts/*.md"
+    }))
+    .use(markdown())
+    .use(layouts())
